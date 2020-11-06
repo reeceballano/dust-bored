961cd39d6744ca65bcb2160ba98ab1d6540fcb46
Reece <trazhme@gmail.com>
2020-11-06 22:46:10 +0800

    find single user

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,3 +3,21 @@ const users     = require('../models/users');
 exports.user_list = (req, res) => {
     res.status(200).json(users);
 }
+
+exports.user_detail = (req, res) => {
+    const id = req.params.id;
+
+    const user = users.find(item => parseInt(item.id) === parseInt(id));
+
+    if(user) {
+        return res.status(200).json({
+            success: true,
+            content: user
+        });
+    }
+
+    return res.status(404).json({
+        success: false,
+        content: 'User not found'
+    })
+}
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,10 @@ const express   = require('express');
 const route     = express.Router();
 const userController     = require('../controllers/userController');
 
+// GET ALL USERS
 route.get('/users', userController.user_list);
+
+// GET SINGLE USER
+route.get('/users/:id', userController.user_detail);
 
 module.exports = route;
