415bd97be2f8e390cf9c938acb1b57d4619c3133
Reece <trazhme@gmail.com>
2020-11-06 22:23:56 +0800

    renamed fake_data to models, added controllers

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express       = require('express');
 const app           = express();
 const cors          = require('cors');
-const users          = require('./routes/users');
+const users          = require('./routes/user');
 
 app.use(express.json());
 app.use(cors());
