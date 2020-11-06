c958cd9750f71614b65bd9507a76712cb3a280da
Reece <trazhme@gmail.com>
2020-11-06 22:04:26 +0800

    added router

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express       = require('express');
 const app           = express();
 const cors          = require('cors');
+const home          = require('./router/users');
 
 app.use(express.json());
 app.use(cors());
+
+// API
+app.use('/', (req, res) => {
+    res.send('Homepage');
+})
 
 // START SERVER
 const port = process.env.PORT || 3000;
